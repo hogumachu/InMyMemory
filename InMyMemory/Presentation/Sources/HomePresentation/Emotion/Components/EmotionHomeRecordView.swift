131477a52051541240984e5fc91b4c19b94d26e8
hogumachu <castle_jun@daum.net>
2023-12-30 22:46:17 +0900

    feat: Emotion Record UI 추가

---
+++ InMyMemory/Presentation/Sources/HomePresentation/Emotion/Components/EmotionHomeRecordView.swift
@@ -0,0 +1,47 @@
+//
+//  EmotionHomeRecordView.swift
+//
+//
+//  Created by 홍성준 on 12/30/23.
+//
+
+import UIKit
+import BasePresentation
+import DesignKit
+import SnapKit
+import Then
+
+final class EmotionHomeRecordView: BaseView {
+    
+    private let titleView = MemoryHomeTitleView()
+    private let recordButton = ActionButton()
+    
+    override func setupLayout() {
+        addSubview(titleView)
+        titleView.snp.makeConstraints { make in
+            make.top.trailing.equalToSuperview()
+            make.leading.equalToSuperview().offset(20)
+        }
+        
+        addSubview(recordButton)
+        recordButton.snp.makeConstraints { make in
+            make.top.equalTo(titleView.snp.bottom).offset(10)
+            make.leading.equalToSuperview().offset(20)
+            make.bottom.equalToSuperview()
+            make.size.equalTo(CGSize(width: 120, height: 40))
+        }
+    }
+    
+    override func setupAttributes() {
+        titleView.do {
+            $0.title = "지금 기분이 어떠신가요?"
+        }
+        
+        recordButton.do {
+            $0.style = .border
+            $0.setTitle("기록하기", for: .normal)
+            $0.layer.cornerRadius = 20
+        }
+    }
+    
+}
--- InMyMemory/Presentation/Sources/HomePresentation/Emotion/EmotionHomeViewController.swift
+++ InMyMemory/Presentation/Sources/HomePresentation/Emotion/EmotionHomeViewController.swift
@@ -13,4 +13,56 @@ import RxCocoa
 import SnapKit
 import Then
 
-final class EmotionHomeViewController: UIViewController {}
+final class EmotionHomeViewController: UIViewController {
+    
+    
+    private let scrollView = UIScrollView()
+    private let stackView = UIStackView()
+    
+    private let recordView = EmotionHomeRecordView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+        setupAttributes()
+    }
+    
+    private func setupLayout() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        scrollView.addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        stackView.addArrangedSubview(recordView)
+    }
+    
+    private func setupAttributes() {
+        view.backgroundColor = .background
+        
+        scrollView.do {
+            $0.showsHorizontalScrollIndicator = false
+            $0.showsVerticalScrollIndicator = false
+            $0.contentInset = .init(
+                top: 40,
+                left: 0,
+                bottom: 40,
+                right: 0
+            )
+        }
+        
+        stackView.do {
+            $0.axis = .vertical
+            $0.alignment = .fill
+            $0.distribution = .equalSpacing
+            $0.spacing = 40
+        }
+    }
+    
+}
