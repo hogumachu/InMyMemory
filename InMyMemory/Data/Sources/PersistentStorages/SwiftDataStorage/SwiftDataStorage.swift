e0f7ade638abbbe3a6fa03be40a2477b935db60a
hogumachu <castle_jun@daum.net>
2023-12-31 16:29:14 +0900

    chore: Storage remove 추가

--- InMyMemory/Data/Sources/PersistentStorages/SwiftDataStorage/SwiftDataStorage.swift
+++ InMyMemory/Data/Sources/PersistentStorages/SwiftDataStorage/SwiftDataStorage.swift
@@ -21,6 +21,7 @@ public protocol SwiftDataStorageInterface: AnyObject {
     func read<T: PersistentModel>(predicate: Predicate<T>, sortBy: [SortDescriptor<T>]) -> Single<[T]>
     func readOne<T: PersistentModel>(predicate: Predicate<T>) -> Single<T?>
     func removeAll<T: PersistentModel>(model: T.Type) -> Single<Void>
+    func remove<T: PersistentModel>(model: T) -> Single<Void>
     func insert<T: PersistentModel>(model: T) -> Single<Void>
     func upsert<T: PersistentModel>(model: T, predicate: Predicate<T>) -> Single<Void>
     
@@ -257,6 +258,23 @@ open class SwiftDataStorage: SwiftDataStorageInterface {
                 } catch {
                     single(.failure(error))
                 }
+            }
+            
+            return Disposables.create {
+                request.cancel()
+            }
+        }
+    }
+    
+    open func remove<T: PersistentModel>(model: T) -> Single<Void> {
+        return Single<Void>.create { single -> Disposable in
+            let request = Task { [weak self] in
+                guard let self else {
+                    single(.failure(SwiftDataStorageError.canceled))
+                    return
+                }
+                await remove(model: model)
+                single(.success(()))
             }
             
             return Disposables.create {
