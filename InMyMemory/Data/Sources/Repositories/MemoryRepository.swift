040c243bab365e618388d852322254fb1beef028
hogumachu <castle_jun@daum.net>
2024-01-01 14:44:13 +0900

    feat: MemoryRepository 추가

---
+++ InMyMemory/Data/Sources/Repositories/MemoryRepository.swift
@@ -0,0 +1,54 @@
+//
+//  MemoryRepository.swift
+//
+//
+//  Created by 홍성준 on 1/1/24.
+//
+
+import Foundation
+import Entities
+import Interfaces
+import PersistentStorages
+import RxSwift
+
+public final class MemoryRepository: MemoryRepositoryInterface {
+    
+    private let storage: SwiftDataStorageInterface
+    
+    public init(storage: SwiftDataStorageInterface) {
+        self.storage = storage
+    }
+    
+    public func create(memory: Memory) -> Single<Void> {
+        let model = MemoryModel(memory: memory)
+        return storage.insert(model: model)
+    }
+    
+    public func read(memoryID: UUID) -> Single<Memory?> {
+        let predicate: Predicate<MemoryModel> = #Predicate { model in
+            model.id == memoryID
+        }
+        return storage.readOne(predicate: predicate)
+            .map { $0?.toEntity() }
+    }
+    
+    public func read(greaterThan date: Date) -> Single<[Memory]> {
+        let predicate: Predicate<MemoryModel> = #Predicate { model in
+            model.date > date
+        }
+        let sortBy: SortDescriptor<MemoryModel> = .init(\.updatedAt, order: .forward)
+        return storage.read(predicate: predicate, sortBy: [sortBy])
+            .map { $0.map { $0.toEntity() }}
+    }
+    
+    public func update(memory: Memory) -> Single<Void> {
+        let model = MemoryModel(memory: memory)
+        return storage.insert(model: model)
+    }
+    
+    public func delete(memory: Memory) -> Single<Void> {
+        let model = MemoryModel(memory: memory)
+        return storage.remove(model: model)
+    }
+    
+}
