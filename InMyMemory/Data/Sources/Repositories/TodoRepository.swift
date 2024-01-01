a01a8e2751b26058b281c4bdb15260d483d9fe90
SungJun Hong <74225754+hogumachu@users.noreply.github.com>
2024-01-01 14:57:02 +0900

    할 일 Repository 추가 (#31)
    * chore: Todo Data Model 추가
    
    * feat: TodoRepository 추가

---
+++ InMyMemory/Data/Sources/Repositories/TodoRepository.swift
@@ -0,0 +1,54 @@
+//
+//  TodoRepository.swift
+//
+//
+//  Created by 홍성준 on 1/1/24.
+//
+
+import Foundation
+import Entities
+import Interfaces
+import PersistentStorages
+import RxSwift
+
+public final class TodoRepository: TodoRepositoryInterface {
+    
+    private let storage: SwiftDataStorageInterface
+    
+    public init(storage: SwiftDataStorageInterface) {
+        self.storage = storage
+    }
+    
+    public func create(todo: Todo) -> Single<Void> {
+        let model = TodoModel(todo: todo)
+        return storage.insert(model: model)
+    }
+    
+    public func read(todoID: UUID) -> Single<Todo?> {
+        let predicate: Predicate<TodoModel> = #Predicate { model in
+            model.id == todoID
+        }
+        return storage.readOne(predicate: predicate)
+            .map { $0?.toEntity() }
+    }
+    
+    public func read(greaterThan date: Date) -> Single<[Todo]> {
+        let predicate: Predicate<TodoModel> = #Predicate { model in
+            model.date > date
+        }
+        let sortBy: SortDescriptor<TodoModel> = .init(\.updatedAt, order: .forward)
+        return storage.read(predicate: predicate, sortBy: [sortBy])
+            .map { $0.map { $0.toEntity() }}
+    }
+    
+    public func update(todo: Todo) -> Single<Void> {
+        let model = TodoModel(todo: todo)
+        return storage.insert(model: model)
+    }
+    
+    public func delete(todo: Todo) -> Single<Void> {
+        let model = TodoModel(todo: todo)
+        return storage.remove(model: model)
+    }
+    
+}
---
+++ InMyMemory/Domain/Sources/Interfaces/Repositories/TodoRepositoryInterface.swift
@@ -0,0 +1,18 @@
+//
+//  TodoRepositoryInterface.swift
+//
+//
+//  Created by 홍성준 on 1/1/24.
+//
+
+import Foundation
+import Entities
+import RxSwift
+
+public protocol TodoRepositoryInterface: AnyObject {
+    func create(todo: Todo) -> Single<Void>
+    func read(todoID: UUID) -> Single<Todo?>
+    func read(greaterThan date: Date) -> Single<[Todo]>
+    func update(todo: Todo) -> Single<Void>
+    func delete(todo: Todo) -> Single<Void>
+}
