c23305dada2b78a47c4cf97a4eb6f6f899f775ab
hogumachu <castle_jun@daum.net>
2024-01-12 22:16:07 +0900

    chore: Normal small button style 추가

--- InMyMemory/Presentation/Sources/DesignKit/DesignSystem/Buttons/ActionButtonStyle.swift
+++ InMyMemory/Presentation/Sources/DesignKit/DesignSystem/Buttons/ActionButtonStyle.swift
@@ -11,52 +11,53 @@ public enum ActionButtonStyle {
     
     case normal
     case border
+    case normalSmall
     
     var textColor: UIColor {
         switch self {
-        case .normal: return .background
+        case .normal, .normalSmall: return .background
         case .border: return .orange1
         }
     }
     
     var disabledTextColor: UIColor {
         switch self {
-        case .normal: return .background
+        case .normal, .normalSmall: return .background
         case .border: return .lightGray
         }
     }
     
     var backgroundColor: UIColor {
         switch self {
-        case .normal: return .orange1
+        case .normal, .normalSmall: return .orange1
         case .border: return .background
         }
     }
     
     var disabledBackgroundColor: UIColor {
         switch self {
-        case .normal: return .orange2
+        case .normal, .normalSmall: return .orange2
         case .border: return .darkGray
         }
     }
     
     var pressedBackgroundColor: UIColor {
         switch self {
-        case .normal: return .black.withAlphaComponent(0.3)
+        case .normal, .normalSmall: return .black.withAlphaComponent(0.3)
         case .border: return .black.withAlphaComponent(0.3)
         }
     }
     
     var borderWidth: CGFloat {
         switch self {
-        case .normal: return 0
+        case .normal, .normalSmall: return 0
         case .border: return 1
         }
     }
     
     var borderColor: UIColor {
         switch self {
-        case .normal: return .clear
+        case .normal, .normalSmall: return .clear
         case .border: return .orange1
         }
     }
@@ -69,6 +70,7 @@ public enum ActionButtonStyle {
         switch self {
         case .normal: return .gmarketSans(type: .medium, size: 21)
         case .border: return .gmarketSans(type: .medium, size: 21)
+        case .normalSmall: return .gmarketSans(type: .medium, size: 14)
         }
     }
     
