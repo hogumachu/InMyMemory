5ed0af0e875425839c62326a87aa0450b6a6dbe3
SungJun Hong <74225754+hogumachu@users.noreply.github.com>
2024-01-17 17:26:09 +0900

    #62 test/calendar (#63)
    * chore: Calendar Date 주입 받도록 수정
    
    * test: Calendar Test 추가

---
+++ InMyMemory/Presentation/Tests/CalendarPresentationTests/CalendarReactorTests.swift
@@ -0,0 +1,139 @@
+//
+//  CalendarReactorTests.swift
+//
+//
+//  Created by 홍성준 on 1/17/24.
+//
+
+@testable import CalendarPresentation
+import CalendarTestSupport
+import Entities
+import Interfaces
+import CoreKit
+import XCTest
+import Quick
+import Nimble
+import ReactorKit
+import RxSwift
+
+final class CalendarReactorTests: QuickSpec {
+    
+    override class func spec() {
+        var sut: CalendarHomeReactor!
+        var useCase: CalendarUseCaseMock!
+        var date: Date!
+        
+        describe("CalendarReactor 테스트") {
+            beforeEach {
+                useCase = .init()
+                date = Date()
+                sut = .init(useCase: useCase, date: date)
+            }
+            
+            context("초기화 되면") {
+                it("날짜에 맞는 년/월 타이틀이 생성이 된다.") {
+                    expect { sut.currentState.monthTitle } == "\(date.year)년 \(date.month)월"
+                }
+                
+                it("해당 날짜의 Day가 선택이 된다.") {
+                    expect { sut.currentState.selectDay } == date.day
+                }
+            }
+            
+            context("ViewDidLoad가 되면") {
+                let selectDay = 10
+                let emotionNote = "emotionNote"
+                let memoryNote = "memoryNote"
+                let todoNote = "todoNote"
+                var mockDays: [Day]!
+                
+                beforeEach {
+                    let selectDate = date.replace(day: selectDay)
+                    mockDays = [.init(
+                        metadata: .init(
+                            date: selectDate,
+                            number: selectDay,
+                            isValid: true
+                        ),
+                        items: [
+                            .emotion(.init(note: emotionNote, emotionType: .bad, date: selectDate)),
+                            .memory(.init(images: [], note: memoryNote, date: selectDate)),
+                            .todo(.init(id: UUID(), note: todoNote, isCompleted: false, date: selectDate))
+                        ])
+                    ]
+                    useCase.fetchDaysInMonthYearMonthDays = mockDays
+                    sut.action.onNext(.viewDidLoad)
+                }
+                
+                it("UseCase를 통해 Day 정보를 가져온다.") {
+                    expect { sut.currentState.days.map { $0.metadata.date } } == mockDays.map { $0.metadata.date }
+                }
+                
+                context("날짜가 선택이 되면") {
+                    beforeEach {
+                        sut.action.onNext(.dayDidTap(selectDay))
+                    }
+                    
+                    it("해당 날짜에 대한 감정, 기억, 할일 정보를 가져온다.") {
+                        let notes = sut.currentState.calendarListSections.flatMap(\.items)
+                            .map { item in
+                                switch item {
+                                case .emotion(let model): return model.note
+                                case .memory(let model): return model.note
+                                case .todo(let model): return model.note
+                                }
+                            }
+                        expect { notes.contains(emotionNote) } == true
+                        expect { notes.contains(memoryNote) } == true
+                        expect { notes.contains(todoNote) } == true
+                    }
+                }
+            }
+            
+            context("이전 달이 선택이 되면") {
+                var nextDate: Date!
+                
+                beforeEach {
+                    nextDate = sut.initialState.date.monthsAgo(value: 1)
+                    sut.action.onNext(.monthLeftDidTap)
+                }
+                
+                it("선택된 날짜를 없앤다.") {
+                    expect { sut.currentState.selectDay } == nil
+                }
+                
+                it("날짜에 맞는 년/월 타이틀이 생성이 된다.") {
+                    expect { sut.currentState.monthTitle } == "\(nextDate.year)년 \(nextDate.month)월"
+                }
+                
+                it("UseCase를 통해 Day 정보를 가져온다.") {
+                    expect { useCase.fetchDaysInMonthYearMonthYear } == nextDate.year
+                    expect { useCase.fetchDaysInMonthYearMonthMonth } == nextDate.month
+                }
+            }
+            
+            context("다음 달이 선택이 되면") {
+                var nextDate: Date!
+                
+                beforeEach {
+                    nextDate = sut.initialState.date.monthsAgo(value: -1)
+                    sut.action.onNext(.monthRightDidTap)
+                }
+                
+                it("선택된 날짜를 없앤다.") {
+                    expect { sut.currentState.selectDay } == nil
+                }
+                
+                it("날짜에 맞는 년/월 타이틀이 생성이 된다.") {
+                    expect { sut.currentState.monthTitle } == "\(nextDate.year)년 \(nextDate.month)월"
+                }
+                
+                it("UseCase를 통해 Day 정보를 가져온다.") {
+                    expect { useCase.fetchDaysInMonthYearMonthYear } == nextDate.year
+                    expect { useCase.fetchDaysInMonthYearMonthMonth } == nextDate.month
+                }
+            }
+        }
+    }
+    
+}
