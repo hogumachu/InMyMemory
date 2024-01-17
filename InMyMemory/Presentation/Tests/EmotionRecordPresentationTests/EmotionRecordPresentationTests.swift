03131637a0b76df4abf11adc877cd05be558240e
hogumachu <castle_jun@daum.net>
2024-01-18 00:07:06 +0900

    test: EmotionRecord Test 추가

---
+++ InMyMemory/Presentation/Tests/EmotionRecordPresentationTests/EmotionRecordPresentationTests.swift
@@ -0,0 +1,73 @@
+//
+//  EmotionRecordPresentationTests.swift
+//
+//
+//  Created by 홍성준 on 1/17/24.
+//
+
+@testable import EmotionRecordPresentation
+import EmotionRecordTestSupport
+import Entities
+import Interfaces
+import CoreKit
+import BasePresentation
+import XCTest
+import Quick
+import Nimble
+import ReactorKit
+import RxSwift
+import RxRelay
+import RxFlow
+
+final class EmotionRecordPresentationTests: QuickSpec {
+    
+    override class func spec() {
+        var sut: EmotionRecordReactor!
+        var useCase: EmotionRecordUseCaseMock!
+        var disposeBag: DisposeBag!
+        var stepBinder: EmotionRecordStepBinder!
+        
+        describe("EmotionRecordReactor 테스트") {
+            beforeEach {
+                useCase = .init()
+                sut = .init(useCase: useCase)
+                disposeBag = .init()
+                stepBinder = .init()
+                sut.steps
+                    .bind(to: stepBinder.step)
+                    .disposed(by: disposeBag)
+            }
+            
+            context("감정이 선택되면") {
+                let emotionType: EmotionType = .good
+                
+                beforeEach {
+                    sut.action.onNext(.buttonDidTap(emotionType))
+                }
+                
+                it("해당 감정을 가지고 라우팅 한다.") {
+                    let step = try unwrap(stepBinder.steps.first as? AppStep)
+                    expect {
+                        guard case .emotionRecordNoteIsRequired(let type) = step else {
+                            return .failed(reason: "올바르지 않은 라우팅")
+                        }
+                        return type == emotionType ? .succeeded : .failed(reason: "올바르지 않은 감정 타입")
+                    }.to(succeed())
+                }
+            }
+        }
+    }
+    
+}
+
+final class EmotionRecordStepBinder {
+    
+    var step: Binder<Step> {
+        return Binder(self) { this, step in
+            this.steps.append(step)
+        }
+    }
+    
+    var steps: [Step] = []
+    
+}
