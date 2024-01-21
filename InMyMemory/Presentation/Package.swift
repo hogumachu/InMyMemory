5cf9bd8c68bf17a5087c4b9f35d98e7d55fd2589
hogumachu <castle_jun@daum.net>
2024-01-21 19:07:43 +0900

    test: TodoRecord Snapshot 테스트 추가

--- InMyMemory/Presentation/Package.swift
+++ InMyMemory/Presentation/Package.swift
@@ -545,5 +545,20 @@ let package = Package(
                 .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
             ]
         ),
+        .testTarget(
+            name: "TodoRecordPresentationTests",
+            dependencies: [
+                "PresentationTestSupport",
+                "TodoRecordPresentation",
+                .product(name: "CoreKit", package: "Shared"),
+                .product(name: "Entities", package: "Domain"),
+                .product(name: "Interfaces", package: "Domain"),
+                .product(name: "UseCases", package: "Domain"),
+                .product(name: "DomainTestSupport", package: "Domain"),
+                "Quick",
+                "Nimble",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
+            ]
+        ),
     ]
 )
